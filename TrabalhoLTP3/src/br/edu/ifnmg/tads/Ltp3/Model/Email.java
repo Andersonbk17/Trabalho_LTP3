f6f516627263ba3ef45b78c99249ac4ebcb45323
anderson <anderson@localhost>
2013-04-11 13:47:43 -0300

    Criação da classe Email

---
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/Ltp3/Model/Email.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.Ltp3.Model;
+
+/**
+ *
+ * @author anderson
+ */
+public class Email {
+    private int id;
+    private String endereco;
+    
+    public Email(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" + "endereco=" + endereco + '}';
+    }
+    
+    
+            
+}
