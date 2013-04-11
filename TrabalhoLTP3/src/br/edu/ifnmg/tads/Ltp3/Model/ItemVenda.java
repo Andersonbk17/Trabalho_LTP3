e67ff12ad274e936bf56cc7865c21e4d6b17b729
anderson <anderson@localhost>
2013-04-11 17:46:33 -0300

    Criação da classe ItemVenda

---
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/Ltp3/Model/ItemVenda.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.Ltp3.Model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author anderson
+ */
+public class ItemVenda {
+    private int id;
+    private int quantidade;
+    private Produto produto;
+    
+    /*-------------Construtores--------------------*/
+    
+    public ItemVenda(){}
+    
+     public ItemVenda(int id, int quantidade, Produto produto) {
+        this.id = id;
+        this.quantidade = quantidade;
+        this.produto = produto;
+    }
+    /*---------------------------------------------------*/
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemVenda{" + "quantidade=" + quantidade + ", produto=" + produto + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + this.id;
+        hash = 41 * hash + this.quantidade;
+        hash = 41 * hash + Objects.hashCode(this.produto);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ItemVenda other = (ItemVenda) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.quantidade != other.quantidade) {
+            return false;
+        }
+        if (!Objects.equals(this.produto, other.produto)) {
+            return false;
+        }
+        return true;
+    }
+
+   
+    
+    
+    
+}
