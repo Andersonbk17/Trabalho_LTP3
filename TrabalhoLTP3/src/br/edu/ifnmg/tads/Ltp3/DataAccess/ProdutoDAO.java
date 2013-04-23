7169db358fb5e84be8b094da968e2de993a7206c
aluno <aluno@aluno-desktop>
2013-04-23 11:06:34 -0300

    comentario na classe produtoDAO

--- TrabalhoLTP3/src/br/edu/ifnmg/tads/Ltp3/DataAccess/ProdutoDAO.java
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/Ltp3/DataAccess/ProdutoDAO.java
@@ -4,6 +4,10 @@
  */
 package br.edu.ifnmg.tads.Ltp3.DataAccess;
 
+import br.edu.ifnmg.tads.Ltp3.Model.Produto;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author aluno
@@ -18,6 +22,103 @@ public class ProdutoDAO {
     /*#################################*/
     
     
-    public boolean Salvar(Produto obj){}
+    public boolean Salvar(Produto obj){
+        try{
+            if(obj.getId() == 0){
+                PreparedStatement comando = bd.getConexao()
+                        .prepareStatement("INSERT INTO produtos (nome,descricao,valorUnidadeCompra,valorUnidadeVenda) VALUES (?,?,?,?)");
+                comando.setString(1, obj.getNome());
+                comando.setString(2, obj.getDescricao());
+                comando.setDouble(3, obj.getValorUnidadeCompra());
+                comando.setDouble(4,obj.getValorUnidadeVenda());
+                
+                // falta inserir o estoque            
+            }else{
+            
+                PreparedStatement comando = bd.getConexao()
+                        .prepareStatement("UPDATE");
+            
+            
+            }
+        
+        
+        }catch(SQLException ex){}
+        
+        
+        
+        
+        
+        
+        return false;
+    
+    
+    
+    }
     
 }
+
+
+
+/*
+ 
+ * salvar
+ * 
+ * for (Itemveda iv: itens){
+ *  if(iv.getAtivo() == true){
+ *      if(iv.getid() == 0){
+ *          insert ...
+ * }else{
+ *          update
+ * 
+ * }else{
+ *          delete
+ * 
+ * }
+ * }
+ * }
+ 
+ * 
+ * abrir
+ * 
+ * 
+ * preparedStatemat cmd = con.prepareStatement("select * from itensVendas where venda = ?");
+ * cmd.setInt(1,id);
+ * ResultSet res = cmd.executeQuery();
+ * List<IntemVenda> its = new LinkedList<ItenVenda>();
+ * while(res.next()){
+ *  itemVenda tmp = ...
+ * 
+ * its.add(tmp);
+ * 
+ * }
+ 
+ *venda.setItens(its);
+ 
+ 
+ 
+ */
+
+
+//inserir com cliente
+
+/*
+ 
+ resultset resultado = comando.execulteQuery();
+ * resultado.next();
+ * venda tmp = new venda();
+ * tmp.setId(resultado.getId('id'));
+ * 
+ * 
+ * clientedao cli = new clientedao();
+ * tmp.setcliente(cli.abrir(resultado.getInt("cliente")));
+ * 
+ * 
+ * 
+ * 
+ * preparedStatement cmd = con.preapreStatemant(insert into venda(cliente) values(?....?))
+ * cmd.setInt(obj.getVenda().getId());
+ 
+ 
+ 
+ 
+ */
