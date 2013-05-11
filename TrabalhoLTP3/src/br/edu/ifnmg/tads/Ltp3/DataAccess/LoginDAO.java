1e1d180b40e2ef5fb4ed7f101524d45babf458ef
modric <modric@localhost>
2013-05-10 22:18:06 -0300

    Criação da Classe LoginDAO

---
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/Ltp3/DataAccess/LoginDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.Ltp3.DataAccess;
+
+import br.edu.ifnmg.tads.Ltp3.Model.ErroValidacaoException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author modric
+ */
+public class LoginDAO {
+    private Bd conexao;
+    
+    public LoginDAO(){
+        this.conexao = new Bd();
+    }
+    
+    public int autenticar(String usuario,String senha) throws ErroValidacaoException{
+        try{
+            PreparedStatement comando = conexao
+                    .getConexao().prepareStatement("SELECT id FROM usuarios WHERE usuario = ? AND senha = ?");
+            comando.setString(1, usuario);
+            comando.setString(2, senha);
+            ResultSet rs = comando.executeQuery();
+            
+            if(rs == null){
+                throw new ErroValidacaoException("Usuário não encontrado !");
+            }else{
+                return rs.getRow(); //erro verificar
+            }
+        
+        }catch(SQLException ex){
+            throw new ErroValidacaoException(ex.getMessage());
+        }
+    
+    }
+}
