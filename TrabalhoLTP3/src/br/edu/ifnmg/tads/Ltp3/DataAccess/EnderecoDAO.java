47f55e62d059a4063754c916acb6d6d533934215
modric <modric@localhost>
2013-05-13 21:59:44 -0300

    Alteraçõe em EnderecoDAO

--- TrabalhoLTP3/src/br/edu/ifnmg/tads/Ltp3/DataAccess/EnderecoDAO.java
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/Ltp3/DataAccess/EnderecoDAO.java
@@ -19,24 +19,59 @@ public class EnderecoDAO {
         banco = new Bd();
     }
     
-    public boolean Salvar(Endereco obj){
+    public boolean Salvar(Endereco obj,int idDoBanco){
         try{
             if(obj.getId() == 0){
                 PreparedStatement comando = banco.getConexao()
-                        .prepareStatement("INSERT INTO enderecos (rua,cep,bairro,numero,id_pessoa,id_cidade,id_estado)");
+                        .prepareStatement("INSERT INTO enderecos (rua,cep,bairro,numero,id_pessoa,cidade,estado)");
+                comando.setString(1, obj.getRua());
+                comando.setString(2, obj.getCep());
+                comando.setString(3, obj.getBairro());
+                comando.setString(4, String.valueOf(obj.getNumero()));
+                comando.setInt(5, idDoBanco);
+                comando.setString(6, obj.getCidade());
+                comando.setString(7, obj.getEstado());
+                
+                comando.executeUpdate();
+                comando.getConnection().commit();
             }
+            
+            return true;
         }catch(SQLException ex){
-        
+            ex.printStackTrace();
+            return false;
         }
-    return true;
+  
     
-    /*
-     
-     * falta implementar tudo
-     
-     
-     */
+    }
+    
+    public boolean Apagar(Endereco obj){
+        try{
+            PreparedStatement comando = banco.getConexao()
+                    .prepareStatement("DELETE FROM enderecos WHERE id= ?");
+            comando.setInt(1, obj.getId());
+            comando.getConnection().commit();
+            return true;
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }
+    
+    }
+    
+    public boolean ApagarTodosQuandoExcluiPessoa(int id){
+        try{
+            PreparedStatement comando = banco.getConexao()
+                    .prepareStatement("DELETE FROM enderecos WHERE id_pessoa= ?");
+            comando.setInt(1, id);
+            comando.getConnection().commit();
+            return true;
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }
     
     }
     
 }
+
